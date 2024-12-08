475348594b7f15836af2470d54f5912c6c21abdb
Яровицын Максим Игоревич <maksim.yarovitsyn@simbirsoft.com>
2024-12-08 21:13:47 +0400

    first commit

---
+++ src/ForestersPizzeriaApplication.java
@@ -0,0 +1,12 @@
+public class
+ForestersPizzeriaApplication {
+    public static void main(String[] args) {
+        execute();
+    }
+
+    private static void execute() {
+        var display = new MainDisplay();
+
+        display.executeDisplay();
+    }
+}
---
+++ src/MainDisplay.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MainDisplay {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void executeDisplay() {
+
+    }
+}
